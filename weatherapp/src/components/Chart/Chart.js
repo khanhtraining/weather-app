e0ee9b1487ceccd88ee266ea4812ef3a18d34ab0
Khanh.Huynh <khanh.huynh@s3corp.com.vn>
2021-07-17 00:30:50 +0700

    update weather chart v1
    (cherry picked from commit 03cb1c9b8c1c003859ebbbf4184ed20963a66d05)

--- weatherapp/src/components/Chart/Chart.js
+++ weatherapp/src/components/Chart/Chart.js
@@ -15,6 +15,45 @@ export const Chart = props => {
     useEffect(() => {
         var lineGenerator = d3.line()
             .curve(d3.curveCardinal);
+        var lineGeneratorStraight = d3.line();
+        //Day 1
+        var pathrectangle_day1_12pm = d3.path();
+        pathrectangle_day1_12pm.rect(0, 180, 70, 50);
+        //
+        var pathrectangle_day1_6am = d3.path();
+        pathrectangle_day1_6am.rect(600, 40, 70, 50);
+        //
+        var pathrectangle_day1_12am = d3.path();
+        pathrectangle_day1_12am.rect(1200, 180, 70, 50);
+        //
+        var pathrectangle_day1_6pm = d3.path();
+        pathrectangle_day1_6pm.rect(1800, 40, 70, 50);
+
+        //Day 2
+        var pathrectangle_day2_12pm = d3.path();
+        pathrectangle_day2_12pm.rect(2400, 180, 70, 50);
+        //
+        var pathrectangle_day2_6am = d3.path();
+        pathrectangle_day2_6am.rect(3000, 40, 70, 50);
+        //
+        var pathrectangle_day2_12am = d3.path();
+        pathrectangle_day2_12am.rect(3600, 180, 70, 50);
+        //
+        var pathrectangle_day2_6pm = d3.path();
+        pathrectangle_day2_6pm.rect(4200, 40, 70, 50);
+
+        //Day 3
+        var pathrectangle_day3_12pm = d3.path();
+        pathrectangle_day3_12pm.rect(4800, 180, 70, 50);
+        //
+        var pathrectangle_day3_6am = d3.path();
+        pathrectangle_day3_6am.rect(5200, 40, 70, 50);
+        //
+        var pathrectangle_day3_12am = d3.path();
+        pathrectangle_day3_12am.rect(5800, 180, 70, 50);
+        //
+        var pathrectangle_day3_6pm = d3.path();
+        pathrectangle_day3_6pm.rect(7200, 40, 70, 50);
 
         const canvasWater = [
             [0, 300],
@@ -118,13 +157,29 @@ export const Chart = props => {
             [6600, 300],
         ];
 
+        const canvasMoon1 = [
+            [0,0],
+            [0,300],
+            [600,300],
+            [600,0]
+        ]
+
+        const canvasMoon2 = [
+            [0,0],
+            [0,300],
+            [600,300],
+            [600,0]
+        ]
+
         const pathData = lineGenerator(canvasWater);
         const pathCanvas1 = lineGenerator(canvas1);
         const pathCanvas2 = lineGenerator(canvas2);
         const pathCanvas3 = lineGenerator(canvas3);
+        const pathMoon = lineGeneratorStraight(canvasMoon1);
 
         d3.select('.pathData')
             .attr('d', pathData);
+
         d3.select('.pathCanvas1')
             .attr('d', pathCanvas1);
         d3.select('.pathCanvas2')
@@ -132,23 +187,57 @@ export const Chart = props => {
         d3.select('.pathCanvas3')
             .attr('d', pathCanvas3);
 
+        d3.select('.pathMoon')
+            .attr('d',pathMoon);
+        //day 1
+        d3.select('.pathrectangle_day1_12pm')
+            .attr('d', pathrectangle_day1_12pm)
+            .attr("fill", '#cfcfcf');
+
+        d3.select('.pathrectangle_day1_6am')
+            .attr('d', pathrectangle_day1_6am)
+            .attr("fill", '#cfcfcf');
+
+        d3.select('.pathrectangle_day1_12am')
+            .attr('d', pathrectangle_day1_12am)
+            .attr("fill", '#cfcfcf');
+
+        d3.select('.pathrectangle_day1_6pm')
+            .attr('d', pathrectangle_day1_6pm)
+            .attr("fill", '#cfcfcf');
+        //day 2
+        d3.select('.pathrectangle_day2_12pm')
+            .attr('d', pathrectangle_day2_12pm)
+            .attr("fill", '#cfcfcf');
+
+        d3.select('.pathrectangle_day2_6am')
+            .attr('d', pathrectangle_day2_6am)
+            .attr("fill", '#cfcfcf');
+
+        d3.select('.pathrectangle_day2_12am')
+            .attr('d', pathrectangle_day2_12am)
+            .attr("fill", '#cfcfcf');
+
+        d3.select('.pathrectangle_day2_6pm')
+            .attr('d', pathrectangle_day2_6pm)
+            .attr("fill", '#cfcfcf');
         // Viết 1 cái Set interval chỗ này cho nó tự gọi lại hàm fetch api, ví dụ 5' hoặc 30s gì đó...vip
         // Fetch API
-        fetchApi();
+        // fetchApi();
     }, []);
     const onResizeWidth = e => {
         const width = window.innerWidth;
         setWindowWidth(width);
     }
     // Get datetime
-    useEffect(() => {
-        if (responseData != null && responseData !== undefined) {
-            const dateTime = new Date(responseData.dt * 1000);
-            console.log(dateTime.getDate());
-            console.log(dateTime.getDay());
-            console.log(dateTime.getFullYear())
-        }
-    }, [responseData]);
+    // useEffect(() => {
+    //     if (responseData != null && responseData !== undefined) {
+    //         const dateTime = new Date(responseData.dt * 1000);
+    //         console.log(dateTime.getDate());
+    //         console.log(dateTime.getDay());
+    //         console.log(dateTime.getFullYear())
+    //     }
+    // }, [responseData]);
 
     // const aaa = setDateTime(date.getDate());
     // console.log(aaa);
@@ -167,41 +256,102 @@ export const Chart = props => {
     }
 
     const formartedData = fakeAPIPromise.map(d => ({
-        date: moment(d.time).format('DD/MM/yyyy'),
+        time: moment(d.time).format('HH:mm'),
         tide: d.tide
     }));
 
     console.log(formartedData)
 
-    useEffect(async () => {
-        const data = await fakeAPIPromise;
-        console.log(data, 'data');
-        //setData(data);
-    }, []);
+    // useEffect(async () => {
+    //     const data = await formartedData;
+    //     console.log(data, 'data');
+    //     //setData(data);
+    // }, []);
 
-    console.log(data);
+    console.log(formartedData[0].tide);
 
     return (
         <div className="svg_chart">
-            <div className="svg_chart-day">
 
-                {/* {formartedData.map(d => [                    <h4>Day
-                        {d.date}
-                        {' - '}
-                        {d.tide}
-                    </h4>
-                ])} */}
+            <div className="svg_chart-day">
 
             </div>
             <svg className="svg">
 
                 <path className='pathData'>
+
                 </path>
                 <path className='pathCanvas1'>
                 </path>
                 <path className='pathCanvas2'>
                 </path>
                 <path className='pathCanvas3'>
+                </path>
+                
+                <path className='pathrectangle_day1_12pm'>
+                </path>
+                <text x="0" y="200" >{formartedData[1].tide} m</text>
+                <text x="0" y="220" >{formartedData[1].time} am</text>
+
+                <path className='pathrectangle_day1_6am'>
+                </path>
+                <text x="624" y="60" >{formartedData[0].tide} m</text>
+                <text x="600" y="80" >{formartedData[0].time} am</text>
+
+                <path className='pathrectangle_day1_12am'>
+                </path>
+                <text x="1200" y="200" >{formartedData[2].tide} m</text>
+                <text x="1200" y="220" >{formartedData[2].time} am</text>
+
+                <path className='pathrectangle_day1_6pm'>
+                </path>
+                <text x="1800" y="60" >{formartedData[3].tide} m</text>
+                <text x="1800" y="80" >{formartedData[3].time} am</text>
+
+
+
+                <path className='pathrectangle_day2_12pm'>
+                </path>
+                <text x="2400" y="200" >{formartedData[4].tide} m</text>
+                <text x="2400" y="220" >{formartedData[4].time} am</text>
+
+                <path className='pathrectangle_day2_6am'>
+                </path>
+                <text x="3000" y="60" >{formartedData[5].tide} m</text>
+                <text x="3000" y="80" >{formartedData[5].time} am</text>
+
+                <path className='pathrectangle_day2_12am'>
+                </path>
+                <text x="3600" y="200" >{formartedData[6].tide} m</text>
+                <text x="3600" y="220" >{formartedData[6].time} am</text>
+
+                <path className='pathrectangle_day2_6pm'>
+                </path>
+                <text x="4200" y="60" >{formartedData[7].tide} m</text>
+                <text x="4200" y="80" >{formartedData[7].time} am</text>
+
+
+                <path className='pathrectangle_day3_12pm'>
+                </path>
+                <text x="4800" y="200" >{formartedData[8].tide} m</text>
+                <text x="4800" y="220" >{formartedData[8].time} am</text>
+
+                <path className='pathrectangle_day3_6am'>
+                </path>
+                <text x="5200" y="60" >{formartedData[9].tide} m</text>
+                <text x="5200" y="80" >{formartedData[9].time} am</text>
+
+                <path className='pathrectangle_day3_12am'>
+                </path>
+                <text x="5800" y="200" >{formartedData[10].tide} m</text>
+                <text x="5800" y="220" >{formartedData[10].time} am</text>
+
+                <path className='pathrectangle_day4_6pm'>
+                </path>
+                <text x="7200" y="60" >{formartedData[11].tide} m</text>
+                <text x="7200" y="80" >{formartedData[11].time} am</text>
+
+                <path className='pathMoon'>
                 </path>
             </svg>
 
--- weatherapp/src/fakeAPIPromise.js
+++ weatherapp/src/fakeAPIPromise.js
@@ -24,4 +24,34 @@
             time: new Date('May 17 2021 12:00'),
             tide: 0.8,
         },
+        
+
+        {
+            time: new Date('May 18 2021 06:00'),
+            tide: 1.2,
+        },
+        {
+            time: new Date('May 18 2021 12:00'),
+            tide: 1.4,
+        },
+        {
+            time: new Date('May 19 2021 06:00'),
+            tide: 2.5,
+        },
+        {
+            time: new Date('May 19 2021 12:00'),
+            tide: 0.5,
+        },
+        {
+            time: new Date('May 20 2021 06:00'),
+            tide: 2.8,
+        },
+        {
+            time: new Date('May 20 2021 12:00'),
+            tide: 0.8,
+        },
+        {
+            time: new Date('May 21 2021 06:00'),
+            tide: 2.8,
+        },
     ]
